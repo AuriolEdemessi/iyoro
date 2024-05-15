80da297107a4349d2b1c6098e5f65844a833436b
Auriol <aurioledemessi7@gmail.com>
2024-05-15 08:04:02 +0100

    06

--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -128,22 +128,22 @@ http://miradontsoa.com
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="gallery.html">Gallery</a>
+              <a class="nav-link" href="gallery.html">Visite Rapide</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="item.html">Item</a>
+              <a class="nav-link" href="item.html">Immersion</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="article.html">Article</a>
+              <a class="nav-link" href="article.html">Histoire</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="demo.html">Demo</a>
+              <a class="nav-link" href="demo.html">Carte</a>
             </li>
           </ul>
           <div class="navbar-desc">
             <div class="row">
               <div class="col col-12 col-lg-8">
-                <p>This is our moto and it is just a placeholder text.
+                <p>Parcourez le Bénin depuis chez vous !
                 </p>
               </div>
             </div>
@@ -178,220 +178,7 @@ http://miradontsoa.com
 
   @yield('content')
 
-    <!-- Begin of register/login/signin section -->
-    <div class="section section-register " data-section="register">
-
-      <div class="section-cover-full bg-color cover-anim-off" data-bgcolor="#3f51b5"></div>
-
-      <!-- Begin of section wrapper -->
-      <div class="section-wrapper with-margin">
-
-        <!-- content -->
-        <div class="section-content anim">
-
-          <div class="row">
-
-            <div class="col-12 col-md-4 text-left">
-              <!-- title and description -->
-              <div class="title-desc pr-4">
-                <h2 class="display-5 anim-2">Call to action.</h2>
-              </div>
-            </div>
-
-            <div class="col-12 col-md-8">
-              <!-- form container form-container-signup-->
-              <div class="form-container-signup">
-
-                <form class="send_email_form form-container" method="post" action="http://demo.highhay.com/agenci/ajaxserver/serverfile.php">
-                  <div class="form-desc">
-                    <p class="invite anim-4">Don't miss any news via email ! Register email if you agree :</p>
-                    <div class="notice">
-                      <span class="asterisk">*</span>you agree to send email</div>
-                  </div>
-                  <div class="form-input anim-5">
-                    <div class="row">
-                      <div class="col-12 col-md-8 col-lg-10">
-                        <div class="form-group form-success-gone">
-                          <label for="reg-email">Email Address *</label>
-                          <input id="reg-email" name="email" class="form-control-line form-control-white" type="email"
-                          required placeholder="your@email.address" data-validation-type="email"
-                          />
-                        </div>
-
-                        <div class="form-group form-success-gone mb-0">
-                          <button id="submit-email" name="submit_email" class="btn btn-outline-white form-success-gone mb-0">
-                            <span class="text">Subscribe</span>
-                          </button>
-                        </div>
-                      </div>
-                      <div class="col-12 col-md-4 col-lg-2">
-                      </div>
-                    </div>
-                    <div class="form-group mb-0">
-                      <div>
-                        <p class="email-ok invisible form-text-feedback form-success-visible">Your email has been registred, thank you.</p>
-                      </div>
-                    </div>
-                  </div>
-                </form>
-                <!-- END send_email_form -->
-              </div>
-              <!-- End of form container form-container-signup-->
-
-            </div>
-          </div>
-        </div>
-        <!-- end of content -->
-      </div>
-      <!-- End of section wrapper -->
-
-    </div>
-    <!-- End of register/login/signin section -->
-
-    <!-- Begin of contact section -->
-    <div class="section section-contact " data-section="contact-info">
-
-      <!-- Begin of section wrapper -->
-      <div class="section-wrapper  with-margin">
-
-        <div class="section-content">
-          <div class="row">
-
-            <!-- begin of title content -->
-            <div class="col-12 col-lg-4 anim">
-              <div class="title-desc pr-4">
-                <h2 class="display-5 anim-2">Let's Talk.</h2>
-              </div>
-
-              <!-- Action button -->
-              <div class="btns-action anim-3 contact-btns-action mb-4">
-                <a class="btn btn-line show-contact-information" href="#">
-                  <span class="text">Information</span>
-                </a>
-                <a class="btn btn-line show-contact-message" href="#">
-                  <span class="text">Message</span>
-                </a>
-              </div>
-            </div>
-            <!-- end of title content -->
-
-            <!-- begin of desc content -->
-            <div class="col-12 col-lg-8">
-              <!-- content -->
-              <div class="section-content anim text-left">
-                <!-- begin of slider -->
-                <div class="slider-swiper-contact">
-                  <!-- slider content-->
-                  <div class="swiper-container slider-container">
-                    <div class="item-list swiper-wrapper">
-                      <!-- contact-information-slide -->
-                      <div class="swiper-slide contact-information-slide">
-                        <div class="contact-information" id="contact-information">
-                          <!-- title and description -->
-                          <div class="title-desc mb-0">
-                            <div class="anim-4">
-                              <p>Want to start an awesome projects or have questions
-                                about our company and products found on our stores?
-                                Feel free to talk to us, we will be happy to answser
-                                you. Here is how to reach us:
-                              </p>
-                            </div>
-                            <div class="address-container anim-5">
-                              <div class="row">
-                                <div class="col-12 col-md-6 col-lg-6">
-                                  <h4 class="display-6 display-decor">Contact</h4>
-                                  <p>Call: 01 234 567 89</p>
-                                  <p>Email: ouremail@domain.com</p>
-
-                                  <div class="icons icons-margin">
-                                    <a class="btn-icon" href="#">
-                                      <i class="icon fa fa-instagram"></i>
-                                    </a>
-                                    <a class="btn-icon" href="#">
-                                      <i class="icon fa fa-twitter"></i>
-                                    </a>
-                                  </div>
-                                </div>
-                                <div class="col-12 col-md-6 col-lg-6">
-                                  <h4 class="display-6 display-decor">Address</h4>
-                                  <p>
-                                    Company address
-                                    <br>12 Street Turning Place
-                                    <br>South Est, Antartica
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <!-- end of contact-information-slide -->
-
-                      <!-- contact-message-slide -->
-                      <div class="swiper-slide contact-message-slide">
-                        <div class="contact-message">
-                          <!-- title and description -->
-                          <div class="title-desc">
-                            <p class="">For questions about our company and products found on
-                              our stores. Cras vitae neque molestie, rhoncus ipsum
-                              sit amet, lobortis dui. Fusce in urna sem.</p>
-                          </div>
-                          <!-- begin of message container -->
-                          <div class="message-form">
-                            <div class="form-container form-container-card">
-                              <!-- Message form container -->
-                              <form class="send_message_form message form" method="post" action="http://demo.highhay.com/agenci/ajaxserver/serverfile.php"
-                              id="message_form">
-                                <div class="row">
-                                  <div class="col-12 col-lg-6">
-                                    <div class="form-group name">
-                                      <input id="mes-name" name="name" type="text" placeholder="Name" class="form-control form-control-outline input-full thick form-success-clean"
-                                      required>
-                                    </div>
-                                    <div class="form-group email">
-                                      <input id="mes-email" type="email" placeholder="Email" name="email" class="form-control form-control-outline input-full thick form-success-clean"
-                                      required>
-                                    </div>
-                                    <div class="form-group no-border">
-                                      <textarea id="mes-text" placeholder="Message ..." name="message" class="form-control form-control-outline thick form-success-clean"
-                                      required></textarea>
-
-                                      <div>
-                                        <p class="message-ok invisible form-text-feedback form-success-visible">Your message has been sent, thank you.</p>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div class="col-12 col-lg-6">
-                                    <button id="submit-message" class="btn btn-outline-white email_b" name="submit_message">
-                                      <span class="txt">Send Now</span>
-                                      <span class="arrow-icon"></span>
-                                    </button>
-                                  </div>
-                                </div>
-
-                              </form>
-                            </div>
-                          </div>
-                          <!-- end of message container -->
-                        </div>
-                      </div>
-                      <!-- end of contact-message-slide -->
-
-                    </div>
-                  </div>
-                </div>
-                <!-- end of slider slider-swiper-gamma-->
-              </div>
-            </div>
-            <!-- end of desc content -->
-          </div>
-        </div>
-
-      </div>
-      <!-- End of section wrapper -->
-
-    </div>
-    <!-- End of contact section -->
+    
   </main>
   <!-- END OF page main content -->
 
